78dfd61cd6ed9edf9f52a4eaea62222431d7fa3a
“kasunihasara” <kasunih99@gmail.com>
2022-07-11 09:26:16 +0530

    2nd try to push to another repo

---
+++ test/src/main/java/model/newgithub.java
@@ -0,0 +1,44 @@
+package model;
+
+public class newgithub {
+	
+	private int id;
+	private String name;
+	private String email;
+	private String phone;
+	private String usename;
+	private String pass;
+	public newgithub(int id, String name, String email, String phone, String usename, String pass) {
+		
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.usename = usename;
+		this.pass = pass;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getUsename() {
+		return usename;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+}
