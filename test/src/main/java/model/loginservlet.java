436ff3920bbeb9b209d496afdfdee962fe5df600
“kasunihasara” <kasunih99@gmail.com>
2022-07-11 09:23:17 +0530

    this is another try to push in to another repository

---
+++ test/src/main/java/model/loginservlet.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginservlet
+ */
+@WebServlet("/loginservlet")
+public class loginservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+			String username= request.getParameter("uid");
+			String password= request.getParameter("pass");
+			
+			 
+		   			try {
+			List<Customer> cusDetails=customerDBUtil.validate(username, password);
+			request.setAttribute("cusDetails", cusDetails);}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+			RequestDispatcher dis = request.getRequestDispatcher("useraccount.jsp");
+			dis.forward(request, response);
+		}
+		
+	}
+
+
